589dceb1a409b89e7ee9d29f2c2606667f501f24
mahakusaladhamma <tastedanotheruniverse@gmail.com>
2026-07-24 10:40:10 +0200

    Add v0.4 exam mode

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = 'v0.3';
-export const APP_RELEASE_NAME = 'Guided Theory';
+export const APP_VERSION = 'v0.4';
+export const APP_RELEASE_NAME = 'Exam Mode';
 export const DEFAULT_TOPIC_ID = 'overview';
 export const STORAGE_KEY = 'distributedIntelligenceProgress';
