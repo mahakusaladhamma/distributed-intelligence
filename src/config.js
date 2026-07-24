9b36fdb63351069bb12a382d123033a9bb998135
mahakusaladhamma <tastedanotheruniverse@gmail.com>
2026-07-24 10:30:11 +0200

    Add v0.3 guided theory: update src/config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = 'v0.2';
-export const APP_RELEASE_NAME = 'Practice Lineup';
+export const APP_VERSION = 'v0.3';
+export const APP_RELEASE_NAME = 'Guided Theory';
 export const DEFAULT_TOPIC_ID = 'overview';
 export const STORAGE_KEY = 'distributedIntelligenceProgress';
